3947a4c7d345da80d1b0de55a2a822b4ae5a3757
whorishi <0710rishi@gmail.com>
2022-08-10 01:22:33 +0530

    updated minheight of toolbar and removed warnings

--- app/src/main/java/com/example/algoverse/ProfileFragment.java
+++ app/src/main/java/com/example/algoverse/ProfileFragment.java
@@ -2,14 +2,12 @@ package com.example.algoverse;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.fragment.app.Fragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
