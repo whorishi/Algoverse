fd41280a8a6f40f9750746726c5ef0af32eb1e08
whorishi <0710rishi@gmail.com>
2022-09-28 20:38:11 +0530

    fetching data from firestore to profle fragment and updating plugins to 7.3.0

--- app/src/main/java/com/example/algoverse/ProfileFragment.java
+++ app/src/main/java/com/example/algoverse/ProfileFragment.java
@@ -6,15 +6,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
 
     FirebaseAuth mAuth;
     Button logoutButton;
+    FirebaseFirestore fstore;
+    ImageView profileImage;
+    String userID;
+    TextView fname;
+    TextView Email;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -35,7 +49,21 @@ public class ProfileFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         mAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
         FirebaseUser user= mAuth.getCurrentUser();
+        profileImage = view.findViewById(R.id.profileImageId);
+        fname=view.findViewById(R.id.profileNameTextView);
+        Email=view.findViewById(R.id.profileEmailTextVew);
+        userID= Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+        DocumentReference documentReference= fstore.collection("users").document(userID);
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                fname.setText(documentSnapshot.getString("name"));
+                Email.setText(documentSnapshot.getString("email"));
+            }
+        });
+
         logoutButton = view.findViewById(R.id.logoutbutton);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
